7690d585923abb08fab796a2594192d11a553a1c
Nether <trextim@gmx.de>
2024-05-31 20:57:49 +0200

    Version 0.9.8

--- ColorblindSeals.lua
+++ ColorblindSeals.lua
@@ -3,26 +3,35 @@
 --- MOD_ID: ColorblindSeals
 --- MOD_AUTHOR: [Nether]
 --- MOD_DESCRIPTION: Add colorblind-friendly seals to the game!
---- LOADER_VERSION_GEQ: 1.0.0
+--- LOADER_VERSION_LEQ: 0.9.8
 --- VERSION: 1.0.0
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
-SMODS.Atlas{
-    key = "centers",
-    path = "Enhancers-Colorblind.png",
-    px = 71,
-    py = 95,
-    inject = function(self)
-        SMODS.Atlas.inject(self)
-        G.shared_seals = {
-            Gold = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 2,y = 0}),
-            Purple = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 4,y = 4}),
-            Red = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 5,y = 4}),
-            Blue = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 6,y = 4}),
-        }
-    end
-}
+function SMODS.INIT.ColorblindSeals()
 
-sendDebugMessage("Injected colorblind-friendly seals!")
+    local seals = SMODS.Sprite:new(
+        "centers",
+        SMODS.findModByID("ColorblindSeals").path,
+        "Enhancers-Colorblind.png",
+        71,
+        95,
+        "asset_atli"
+    )
+
+    seals.inject = function(self)
+            SMODS.Sprite:inject(self)
+            G.shared_seals = {
+                Gold = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 2,y = 0}),
+                Purple = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 4,y = 4}),
+                Red = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 5,y = 4}),
+                Blue = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 6,y = 4}),
+            }
+        end
+
+    seals:register()
+
+    sendDebugMessage("Injected colorblind-friendly seals!")
+
+end
