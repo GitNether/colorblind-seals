abc47c39042c11bfc0410758487007288b6f53a4
Nether <trextim@gmx.de>
2024-05-31 21:15:13 +0200

    removed inject

--- ColorblindSeals.lua
+++ ColorblindSeals.lua
@@ -10,28 +10,13 @@
 ------------MOD CODE -------------------------
 
 function SMODS.INIT.ColorblindSeals()
-
-    local seals = SMODS.Sprite:new(
+    SMODS.Sprite:new(
         "centers",
         SMODS.findModByID("ColorblindSeals").path,
         "Enhancers-Colorblind.png",
         71,
         95,
         "asset_atli"
-    )
-
-    seals.inject = function(self)
-            SMODS.Sprite:inject(self)
-            G.shared_seals = {
-                Gold = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 2,y = 0}),
-                Purple = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 4,y = 4}),
-                Red = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 5,y = 4}),
-                Blue = Sprite(0, 0, G.CARD_W, G.CARD_H, self, {x = 6,y = 4}),
-            }
-        end
-
-    seals:register()
-
+    ):register()
     sendDebugMessage("Injected colorblind-friendly seals!")
-
 end
